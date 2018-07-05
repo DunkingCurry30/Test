65ed9241aff9ae888bdfb4c6b74184a8d0977129
Sherlock1020 <157468143@qq.com>
2018-07-05 14:53:27 +0800

    Secondtime

---
+++ PrintBook.java
@@ -0,0 +1,13 @@
+package bookmanager;
+
+import javax.swing.*;
+
+/**
+ * Created by Administrator on 2018/3/13 0013.
+ */
+public class PrintBook {
+    public PrintBook(JFrame frame)
+    {
+
+    }
+}
---
+++ SearchName.java
@@ -0,0 +1,14 @@
+package bookmanager;
+
+import javax.swing.*;
+
+/**
+ * Created by Administrator on 2018/3/13 0013.
+ */
+public class SearchName {
+    public SearchName(JPanel panel)
+    {
+
+    }
+
+}
